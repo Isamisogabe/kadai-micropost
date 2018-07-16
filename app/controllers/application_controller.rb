5173e978596b2f3820d3eb1087c3387c9c614a38
isami.sogabe <isami.sogabe@techacademy.jp>
2018-07-16 10:43:58 +0000

    follow, unfollow"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
   private
   
-  def require_user_loggled_in
+  def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,15 @@ Rails.application.routes.draw do
   
   
   get 'signup', to: 'users#new'
-  resources :users, only: [ :index, :show, :new, :create]
+  
+  resources :users, only: [ :index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers
+    end
+  end
+  
+  resources :relationships, only: [ :destroy,  :create]
   
   resources :microposts, only: [:create, :destroy]
   
